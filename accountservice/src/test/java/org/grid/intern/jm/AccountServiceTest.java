5df0c13e6624db422dd45c94f1f5d8b06e9e50be
Shaheim Thomas <tshaheim@gmail.com>
2023-02-22 20:26:14 -0500

    changed test framework to testng

--- accountservice/src/test/java/org/grid/intern/jm/AccountServiceTest.java
+++ accountservice/src/test/java/org/grid/intern/jm/AccountServiceTest.java
@@ -1,17 +1,17 @@
 package org.grid.intern.jm;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertThrows;
+import static   org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 public class AccountServiceTest implements TestData {
+    
     @Test
-//    @Description("Test retrieve an account if found by id")
-//    @Step("1")
     public void testReturnsAccountIfFound() {
         Account found = service.findAccountByOwnerId(89534789);
-        Assertions.assertNotNull(found, "Account not Found");
+        Assert.assertNotNull(found, "Account not Found");
     }
 
     @Test
@@ -19,28 +19,28 @@ public class AccountServiceTest implements TestData {
 //    @Step("2")
     public void testReturnsNullIfAccountNotFound() {
         Account found = service.findAccountByOwnerId(9981090);
-        Assertions.assertNull(found, "Account found");
+        Assert.assertNull(found, "Account found");
     }
 
     @Test
 //    @Description("Test count equals zero when no accounts found exceeding query bal")
 //    @Step("3")
     public void testReturnsZeroIfNoAccountsFoundExceedingBalance() {
-        Assertions.assertEquals(0, service.countAccountsWithBalanceGreaterThan(80000000));
+       assertEquals(0, service.countAccountsWithBalanceGreaterThan(80000000));
     }
 
     @Test
 //    @Description("Test return number of accounts found exceeding query bal")
 //    @Step("4")
     public void testReturnsNumberOfAccountsFoundExceedingBalance() {
-        Assertions.assertEquals(4, service.countAccountsWithBalanceGreaterThan(16000));
+       assertEquals(4, service.countAccountsWithBalanceGreaterThan(16000));
     }
 
     @Test
 //    @Description("Test return number of accounts found exceeding query bal")
 //    @Step("5")
     public void testReturnsAccountsFoundExceedingBalance() {
-        Assertions.assertEquals(3, service.countAccountsWithBalanceGreaterThan(20000));
+       assertEquals(3, service.countAccountsWithBalanceGreaterThan(20000));
     }
 
     @Test
@@ -53,13 +53,10 @@ public class AccountServiceTest implements TestData {
                 new Account(23862890, 4623273, new User(76243831, "Jekyll", "Blake")),
         };
         Exception exception = assertThrowsExactly(IllegalArgumentException.class, () -> new AccountServiceImpl(duplicateAccountIDSet));
-        System.out.println(exception.getMessage());
         assertEquals("  Duplicate Account ID: 23862890 at indices [0] and [2]".trim(), exception.getMessage().trim());
     }
 
     @Test
-//    @Description("Test that Illegal argument exception thrown for duplicate user ids")
-//    @Step("7")
     public void testThrowsIllegalArgumentExceptionDuplicateUserIds() {
         Account[] duplicateUserIDSet = new Account[]{
                 new Account(37653408, 1328745, new User(89568831, "Anthony", "Doe")),
@@ -71,8 +68,6 @@ public class AccountServiceTest implements TestData {
     }
 
     @Test
-//    @Description("Test that Illegal argument exception thrown for duplicate account and duplicate user ids")
-//    @Step("8")
     public void testThrowsIllegalArgumentExceptionDuplicateAccountandUserIds() {
         Account[] duplicateIDSet = new Account[]{
                 new Account(33542908, 1328745, new User(23659034, "Anthony", "Doe")),
