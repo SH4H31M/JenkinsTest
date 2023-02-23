02fc27bba57120cce538c456dcaf76d78f0968ff
Shaheim Thomas <tshaheim@gmail.com>
2023-02-23 10:55:12 -0500

    added versions to pom

--- accountservice/src/test/java/org/grid/intern/jm/AccountServiceTest.java
+++ accountservice/src/test/java/org/grid/intern/jm/AccountServiceTest.java
@@ -1,13 +1,11 @@
 package org.grid.intern.jm;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertThrows;
-import static   org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 public class AccountServiceTest implements TestData {
-    
     @Test
     public void testReturnsAccountIfFound() {
         Account found = service.findAccountByOwnerId(89534789);
